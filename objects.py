2b235343f8e837173366448ccc2904e4b7c00112
Jay C33 <85657500+jayc3-3@users.noreply.github.com>
2023-02-06 14:05:46 -0800

    Updated code to v0.2.0

--- objects.py
+++ objects.py
@@ -2,56 +2,142 @@ import pygame
 
 class WindowIcon():
     def __init__(self):
+        super().__init__()
         self.image = pygame.image.load('./Sprites/icon.png')
-class Player(pygame.sprite.Sprite):
+class WallOne():
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('./Sprites/wall1.png')
+class WallTwo():
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('./Sprites/wall2.png')
+class WallThree():
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('./Sprites/wall3.png')
+class WallFour():
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('./Sprites/wall4.png')
+class WallFive():
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('./Sprites/wall5.png')
+class Stair():
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('./Sprites/stairs.png')
+class Weapon():
+    X = 0
+    Y = 0
+    Rotation = 0
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('./Sprites/sword.png')
+class Wizard():
+    X = 0
+    Y = 0
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/player.png')
-class WoodenSword(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('./Sprites/swordwood.png')
-class ZombieOne(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('./Sprites/zombie.png')
-class ZombieTwo(pygame.sprite.Sprite):
+class ZombieOne():
+    X = 0
+    Y = 0
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/zombie.png')
-class Demon(pygame.sprite.Sprite):
+class ZombieTwo():
+    X = 0
+    Y = 0
+    Alive = False
+    Flipped = False
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('./Sprites/zombie.png')
+class DemonOne():
+    X = 0
+    Y = 0
+    Spawn = 0
+    TimerStart = 0
+    TimerCurrent = 0
+    TimerElapsed = 0
+    Timer = False
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/demon.png')
-class DemonTwo(pygame.sprite.Sprite):
+class DemonTwo():
+    X = 0
+    Y = 0
+    Spawn = 0
+    TimerStart = 0
+    TimerCurrent = 0
+    TimerElapsed = 0
+    Timer = False
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/demon.png')
-class DemonSpawnOne(pygame.sprite.Sprite):
+class DemonspawnOne():
+    X = 0
+    Y = 0
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonSpawnTwo(pygame.sprite.Sprite):
+class DemonspawnTwo():
+    X = 0
+    Y = 0
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonSpawnThree(pygame.sprite.Sprite):
+class DemonspawnThree():
+    X = 0
+    Y = 0
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonSpawnFour(pygame.sprite.Sprite):
+class DemonspawnFour():
+    X = 0
+    Y = 0
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonSpawnFive(pygame.sprite.Sprite):
+class DemonspawnFive():
+    X = 0
+    Y = 0
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonSpawnSix(pygame.sprite.Sprite):
+class DemonspawnSix():
+    X = 0
+    Y = 0
+    Alive = False
+    Flipped = False
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('./Sprites/demonspawn.png')
-class WallFront1:
+class BatOne():
+    X = 0
+    Y = 0
+    AnimFrame = 1
+    Flipped = False
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/wallfront.png')
+        self.frame1 = pygame.image.load('./Sprites/bat_frame1.png')
+        self.frame2 = pygame.image.load('./Sprites/bat_frame2.png')
