689e5b6e8a9e743df7d4eb9e6df6121e54863710
Jay C33 <85657500+jayc3-3@users.noreply.github.com>
2023-02-14 17:19:54 -0800

    Update to v0.2.1

--- objects.py
+++ objects.py
@@ -3,38 +3,47 @@ import pygame
 class WindowIcon():
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/icon.png')
+        
+        self.Image = pygame.image.load('./Sprites/icon.png')
 class WallOne():
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/wall1.png')
+        
+        self.Image = pygame.image.load('./Sprites/wall1.png')
 class WallTwo():
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/wall2.png')
+        
+        self.Image = pygame.image.load('./Sprites/wall2.png')
 class WallThree():
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/wall3.png')
+        
+        self.Image = pygame.image.load('./Sprites/wall3.png')
 class WallFour():
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/wall4.png')
+        
+        self.Image = pygame.image.load('./Sprites/wall4.png')
 class WallFive():
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/wall5.png')
+        
+        self.Image = pygame.image.load('./Sprites/wall5.png')
 class Stair():
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/stairs.png')
+        
+        self.Image = pygame.image.load('./Sprites/stairs.png')
 class Weapon():
     X = 0
     Y = 0
     Rotation = 0
+    
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/sword.png')
+        
+        self.Image = pygame.image.load('./Sprites/sword.png')
 class Wizard():
     X = 0
     Y = 0
@@ -44,18 +53,22 @@ class Wizard():
     DamageTimerElapsed = 0
     Health = 3
     Flipped = False
+    
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/player.png')
-class ZombieOne():
+        
+        self.Image = pygame.image.load('./Sprites/player.png')
+class Zombie():
     X = 0
     Y = 0
     Anim = 0
     AnimFrame = 0
     Alive = False
     Flipped = False
+    
     def __init__(self):
         super().__init__()
+        
         self.CurrentFrame = pygame.image.load('./Sprites/zombie.png')
         self.Alive0 = pygame.image.load('./Sprites/zombie.png')
         self.Dying0 = pygame.image.load('./Sprites/zombie_dying0.png')
@@ -63,23 +76,7 @@ class ZombieOne():
         self.Dying2 = pygame.image.load('./Sprites/zombie_dying2.png')
         self.Dying3 = pygame.image.load('./Sprites/zombie_dying3.png')
         self.Dying4 = pygame.image.load('./Sprites/zombie_dying4.png')
-class ZombieTwo():
-    X = 0
-    Y = 0
-    Anim = 0
-    AnimFrame = 0
-    Alive = False
-    Flipped = False
-    def __init__(self):
-        super().__init__()
-        self.CurrentFrame = pygame.image.load('./Sprites/zombie.png')
-        self.Alive0 = pygame.image.load('./Sprites/zombie.png')
-        self.Dying0 = pygame.image.load('./Sprites/zombie_dying0.png')
-        self.Dying1 = pygame.image.load('./Sprites/zombie_dying1.png')
-        self.Dying2 = pygame.image.load('./Sprites/zombie_dying2.png')
-        self.Dying3 = pygame.image.load('./Sprites/zombie_dying3.png')
-        self.Dying4 = pygame.image.load('./Sprites/zombie_dying4.png')
-class DemonOne():
+class Demon():
     X = 0
     Y = 0
     Health = 2
@@ -92,8 +89,10 @@ class DemonOne():
     SpawnTimer = False
     Alive = False
     Flipped = False
+    
     def __init__(self):
         super().__init__()
+        
         self.CurrentFrame = pygame.image.load('./Sprites/demon.png')
         self.Alive0 = pygame.image.load('./Sprites/demon.png')
         self.Dying0 = pygame.image.load('./Sprites/demon_dying0.png')
@@ -101,77 +100,17 @@ class DemonOne():
         self.Dying2 = pygame.image.load('./Sprites/demon_dying2.png')
         self.Dying3 = pygame.image.load('./Sprites/demon_dying3.png')
         self.Dying4 = pygame.image.load('./Sprites/demon_dying4.png')
-class DemonTwo():
-    X = 0
-    Y = 0
-    Health = 2
-    Anim = 0
-    AnimFrame = 0
-    Spawn = 0
-    SpawnTimerStart = 0
-    SpawnTimerCurrent = 0
-    SpawnTimerElapsed = 0
-    SpawnTimer = False
-    Alive = False
-    Flipped = False
-    def __init__(self):
-        super().__init__()
-        self.CurrentFrame = pygame.image.load('./Sprites/demon.png')
-        self.Alive0 = pygame.image.load('./Sprites/demon.png')
-        self.Dying0 = pygame.image.load('./Sprites/demon_dying0.png')
-        self.Dying1 = pygame.image.load('./Sprites/demon_dying1.png')
-        self.Dying2 = pygame.image.load('./Sprites/demon_dying2.png')
-        self.Dying3 = pygame.image.load('./Sprites/demon_dying3.png')
-        self.Dying4 = pygame.image.load('./Sprites/demon_dying4.png')
-class DemonspawnOne():
+class Demonspawn():
     X = 0
     Y = 0
     Alive = False
     Flipped = False
+    
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonspawnTwo():
-    X = 0
-    Y = 0
-    Alive = False
-    Flipped = False
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonspawnThree():
-    X = 0
-    Y = 0
-    Alive = False
-    Flipped = False
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonspawnFour():
-    X = 0
-    Y = 0
-    Alive = False
-    Flipped = False
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonspawnFive():
-    X = 0
-    Y = 0
-    Alive = False
-    Flipped = False
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('./Sprites/demonspawn.png')
-class DemonspawnSix():
-    X = 0
-    Y = 0
-    Alive = False
-    Flipped = False
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('./Sprites/demonspawn.png')
-class BatOne():
+        
+        self.Image = pygame.image.load('./Sprites/demonspawn.png')
+class Bat():
     X = 0
     Y = 0
     Health = 3
@@ -183,8 +122,10 @@ class BatOne():
     AnimFrame = 0
     Alive = False
     Flipped = False
+
     def __init__(self):
         super().__init__()
+    
         self.CurrentFrame = pygame.image.load('./Sprites/bat_alive0.png')
         self.Anim1Frame0 = pygame.image.load('./Sprites/bat_alive0.png')
         self.Anim1Frame1 = pygame.image.load('./Sprites/bat_alive1.png')
@@ -194,30 +135,9 @@ class BatOne():
         self.Anim3Frame2 = pygame.image.load('./Sprites/bat_dying2.png')
         self.Anim3Frame3 = pygame.image.load('./Sprites/bat_dying3.png')
         self.Anim3Frame4 = pygame.image.load('./Sprites/bat_dying4.png')
-class BatTwo():
-    X = 0
-    Y = 0
-    Health = 3
-    DamageTimer = False
-    DamageTimerStart = 0
-    DamageTimerCurrent = 0
-    DamageTimerElapsed = 0
-    Anim = 0
-    AnimFrame = 0
-    Alive = False
-    Flipped = False
-    def __init__(self):
-        super().__init__()
-        self.CurrentFrame = pygame.image.load('./Sprites/bat_alive0.png')
-        self.Anim1Frame0 = pygame.image.load('./Sprites/bat_alive0.png')
-        self.Anim1Frame1 = pygame.image.load('./Sprites/bat_alive1.png')
-        self.Anim2Frame0 = pygame.image.load('./Sprites/bat_hit.png')
-        self.Anim3Frame0 = pygame.image.load('./Sprites/bat_dying0.png')
-        self.Anim3Frame1 = pygame.image.load('./Sprites/bat_dying1.png')
-        self.Anim3Frame2 = pygame.image.load('./Sprites/bat_dying2.png')
-        self.Anim3Frame3 = pygame.image.load('./Sprites/bat_dying3.png')
-        self.Anim3Frame4 = pygame.image.load('./Sprites/bat_dying4.png')
+
 class WizardHealth():
     def __init__(self):
         super().__init__()
+
         self.Image = pygame.image.load('./Sprites/player.png')
